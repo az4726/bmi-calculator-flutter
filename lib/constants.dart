d4683f7639d9be7c1cf90107ffbe7586b6070cbb
Azhar Zaman <azhar.zaman99@gmail.com>
2020-03-15 12:23:35 +0000

    Correct Colour scheme

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-const Color kInactiveCardColor = Color(0xFF0d101e);
-const Color kActiveCardColor = Color(0xFF101427);
+const Color kInactiveCardColor = Color(0xFF111328);
+const Color kActiveCardColor = Color(0xFF1D1E33);
 const Color kDefaultTextColor = Color(0xFF8D8E98);
 const Color kSelectedTextColor = Color(0xFFFFFFFF);
 const Color kAccentColor = Color(0xFFFF0167);
-const Color kCircleButtonColor = Color(0XFF1C2033);
+const Color kCircleButtonColor = Color(0XFF4C4F5E);
 
 const TextStyle kLabelTextStyle = TextStyle(
   fontSize: 18.0,
